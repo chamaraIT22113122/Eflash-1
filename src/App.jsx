bcc7731255018a64b62fd5ce383e7e99ae107734
Chamara Nuwan <146699721+chamaraIT22113122@users.noreply.github.com>
2026-02-19 13:37:14 +0530

    Remove admin panel and update GitHub Pages deployment artifacts

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { ThemeProvider } from './context/ThemeContext'
 import { ToastProvider } from './context/ToastContext'
-import { AdminProvider } from './context/AdminContext'
 import { SiteContentProvider } from './context/SiteContentContext'
 import { CartProvider } from './context/CartContext'
 import Navbar from './components/Navbar'
@@ -12,7 +11,6 @@ import ScrollToTop from './components/ScrollToTop'
 import LiveChat from './components/LiveChat'
 import Toast from './components/Toast'
 import ScrollProgress from './components/ScrollProgress'
-import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
@@ -22,8 +20,6 @@ import Shop from './pages/Shop'
 import Cart from './pages/Cart'
 import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
-import AdminLogin from './pages/AdminLogin'
-import AdminDashboard from './pages/AdminDashboard'
 import './App.css'
 
 function App() {
@@ -32,47 +28,29 @@ function App() {
       <ToastProvider>
         <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <ThemeProvider>
-            <AdminProvider>
-              <CartProvider>
-                <SiteContentProvider>
-                  <Toast />
-                <Routes>
-                  {/* Admin Routes */}
-                  <Route path="/admin/login" element={<AdminLogin />} />
-                  <Route 
-                    path="/admin/dashboard" 
-                    element={
-                      <ProtectedRoute>
-                        <AdminDashboard />
-                      </ProtectedRoute>
-                    } 
-                  />
-
-                  {/* Public Routes */}
-                  <Route path="/*" element={
-                    <div className="App">
-                      <ScrollProgress />
-                      <Navbar />
-                      <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/services" element={<Services />} />
-                        <Route path="/portfolio" element={<Portfolio />} />
-                        <Route path="/packages" element={<Packages />} />
-                        <Route path="/shop" element={<Shop />} />
-                        <Route path="/cart" element={<Cart />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="*" element={<NotFound />} />
-                      </Routes>
-                      <Footer />
-                      <ScrollToTop />
-                      <LiveChat />
-                    </div>
-                  } />
-                </Routes>
+            <CartProvider>
+              <SiteContentProvider>
+                <Toast />
+                <div className="App">
+                  <ScrollProgress />
+                  <Navbar />
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/packages" element={<Packages />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="*" element={<NotFound />} />
+                  </Routes>
+                  <Footer />
+                  <ScrollToTop />
+                  <LiveChat />
+                </div>
               </SiteContentProvider>
-              </CartProvider>
-            </AdminProvider>
+            </CartProvider>
           </ThemeProvider>
         </Router>
       </ToastProvider>
